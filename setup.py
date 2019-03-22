9a3a96fa09775d4843fec06eaa3e0c2486f267dd
Nzuzo Ndwandwe <nzuzo.ndwandwe@gmail.com>
2019-03-22 16:15:20 +0200

    Setup fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Analse-Hackathon',
+    name='Analyse-Hackathon',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
