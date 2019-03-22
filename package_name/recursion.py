8adeeb979753abd73753fd955fc79bbf1e7d83d3
Nzuzo Ndwandwe <nzuzo.ndwandwe@gmail.com>
2019-03-22 15:14:20 +0200

    Changes to function files

--- package_name/recursion.py
+++ package_name/recursion.py
@@ -1,37 +1,28 @@
 def sum_array(array):
-    '''Return sum of all items in array'''
-    sum = 0
-    for num in array:
-        sum += num
-    return sum;
 
+    ''' Return sum of all items in array'''
 
-def Fibonacci(n):
-    ''' Return nth term in fibonacci sequence'''
-    a = 0
-    b = 1
-    if n < 0:
-        print("Incorrect input")
-    elif n == 0:
-        return a
-    elif n == 1:
-        return b
+    arr = sum(array)
+    return arr
+
+def fibonacci(n):
+
+    '''Return nth term in fibonacci sequence'''
+
+    if n <= 1:
+        return n
     else:
-        for i in range(2,n):
-            c = a + b
-            a = b
-            b = c
-        return b
+        return fibonacci(n-1)+fibonacci(n-2)
 
 def factorial(n):
+
     '''Return n!'''
-    if n <1:   # base case
-       return 1
-   else:
-       returnNumber = n * factorial( n - 1 )  # recursive call
-       print(str(n) + '! = ' + str(returnNumber))
-       return returnNumber
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 def reverse(word):
-     '''Return word in reverse'''
-     return word[-1::-1]
+
+    '''Return word in reverse'''
+    return word[::-1]
